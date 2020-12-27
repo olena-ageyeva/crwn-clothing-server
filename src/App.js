a8c003500061e3603e058ded0234bc5a0b23df00
olena-ageyeva <eageeva@gmail.com>
2020-12-26 19:22:29 -0500

    router setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const Page = ({
+  match: {
+    params: { name },
+  },
+}) => (
+  <div>
+    <h1>{name.toUpperCase()} PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/:name" component={Page} />
+      </Switch>
     </div>
   );
 }
