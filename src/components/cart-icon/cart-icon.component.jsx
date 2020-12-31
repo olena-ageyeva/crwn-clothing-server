767577db6daab4c3767aba3c292d7c071fbb0065
olena-ageyeva <eageeva@gmail.com>
2020-12-30 20:46:57 -0500

    small fix

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -10,11 +10,8 @@ const CartIcon = () => {
   const dispatch = useDispatch();
 
   return (
-    <div className="cart-icon">
-      <ShoppingIcon
-        className="shopping-icon"
-        onClick={() => dispatch(toggleCartHidden())}
-      />
+    <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
+      <ShoppingIcon className="shopping-icon" />
       <span className="item-count">0</span>
     </div>
   );
