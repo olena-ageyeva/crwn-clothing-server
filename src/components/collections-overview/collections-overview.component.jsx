2a487e69b6b1f89162cda47cfcd3578e67ef755a
olena-ageyeva <eageeva@gmail.com>
2020-12-31 23:06:52 -0500

    preview selector added

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectCollections } from "../../store/shop/shop.reducer";
+import { selectCollectionsForPreview } from "../../store/shop/shop.reducer";
 import CollectionPreview from "../collection-preview/collection-preview.component";
 
 import "./collections-overview.styles.scss";
 
 const CollectionsOverview = () => {
-  const collection = useSelector(selectCollections);
+  const collection = useSelector(selectCollectionsForPreview);
   return (
     <div className="collections-overview">
       {collection.map(({ id, ...otherProps }) => (
